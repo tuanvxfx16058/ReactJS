edb5c9a99398179647ebec385286e7ba212599dd
unknown <tuanbk1980@gmail.com>
2022-04-14 00:36:45 +0700

    modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
         <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
         <h1>Vũ Xuân Tuấn </h1>
-        <h2>Đây là bài Lab02.1 </h2>
+        <h2>Đây là bài Lab02.1 push nên nhánh Lab02.1 </h2>
 
 
       </div>
