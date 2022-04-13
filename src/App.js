d4f67a90e474c0316cc37072fbe98a43ce4788a5
unknown <tuanbk1980@gmail.com>
2022-04-14 00:27:02 +0700

    Lab02.1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
+        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
+        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <h1>Vũ Xuân Tuấn </h1>
+        <h2>Đây là bài Lab02.1 </h2>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
