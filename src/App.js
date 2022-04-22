e9cbe10f7681a538803184a9b772f2c31fababab
tuanvxfx16058@funix.edu.vn <tuanvxfx16058@funix.edu.vn>
2022-04-23 01:16:53 +0700

    SPARouter 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap'; 
 import Menu from './components/MenuComponent'
+import Home from '../pages/Home';
+import News from '../pages/News';
+import Contact from '../pages/Contact';
 
 import './App.css';
 class App extends Component {
@@ -13,6 +16,18 @@ class App extends Component {
             <NavbarBrand href="/"> Ristorante Con Fusion  </NavbarBrand>
           </div> 
         </Navbar>
+        
+        <React.Fragment>
+<ul>
+  <li><Home/></li>
+  <li><News/></li>
+
+  <li><Contact/></li>
+
+</ul>
+
+        </React.Fragment>
+        
         <Menu/>
       </div>
     );
