55dabf663965b1f9c0bbf5369020edb3d7d92832
tuanvxfx16058@funix.edu.vn <tuanvxfx16058@funix.edu.vn>
2022-05-12 20:52:27 +0700

    Lab05.3(Single Page Applications Part 1)

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,36 +1,53 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
+import React, { Component } from "react";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 
-class Main extends Component {
+import Menu from "./MenuComponent";
+import Header from "./Header";
+import Footer from "./Footer";
+import Contact from "./ContactComponet";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Homecomponent";
 
+export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      leaders: LEADERS,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
-
   render() {
+    const Homepage = () => {
+      return (
+        <Home
+          dish={this.state.dishes.filter((value) => value.featured)[0]}
+          leader={this.state.leaders.filter((value) => value.featured)[0]}
+          promotion={this.state.promotions.filter((value) => value.featured)[0]}
+        />
+      );
+    };
+
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Header />
+        <Switch>
+          <Route exact path="/home" component={Homepage} />
+          <Route
+            path="/Menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+
+        <Footer />
       </div>
     );
   }
 }
-
-export default Main;
